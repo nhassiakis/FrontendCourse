323117d16e5f24c4beed43a9158e8db8bd502900
nhassiakis <40218955+nhassiakis@users.noreply.github.com>
2019-05-17 12:01:35 +0200

    sad

--- Proj/typescriptTest/greeter.ts
+++ Proj/typescriptTest/greeter.ts
@@ -1,20 +1,7 @@
-class Student{
-    fullName: string;
-    constructor(public firstName:string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+function greeter(person) {
+    return "Hello, " + person;
 }
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-let user = new Student("Jane", "M.", "user");
+let user = "Jane User";
 
 document.body.innerHTML = greeter(user);
-console.log(user);
